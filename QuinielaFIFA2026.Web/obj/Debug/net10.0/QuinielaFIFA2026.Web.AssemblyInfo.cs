be2e3af0d1492e1bc63e3c4a62d648138997cf31
Argenis Villarroel <smitterargenis@gmail.com>
2026-05-24 11:05:14 -0400

    Cambios

--- QuinielaFIFA2026.Web/obj/Debug/net10.0/QuinielaFIFA2026.Web.AssemblyInfo.cs
+++ QuinielaFIFA2026.Web/obj/Debug/net10.0/QuinielaFIFA2026.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuinielaFIFA2026.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d629e52cced5fa158b53f6d55deba354117ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc2104c8a852284c314f8abf59184c3e643326f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuinielaFIFA2026.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuinielaFIFA2026.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
